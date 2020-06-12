ca7068fdee98c461be8bd0bf6778bff2f6be9ca6
sacha <sacha.perdrizat@gmail.com>
2020-06-12 19:59:13 +0200

    correct musician image

--- docker/image-musician/src/musician.js
+++ docker/image-musician/src/musician.js
@@ -1,33 +1,38 @@
 // We use a standard Node.js module to work with UDP
 const dgram = require('dgram');
 
+// We will use uuid package to generate a UUID
+const { v4: uuidv4 } = require('uuid');
+
 const protocol = {
 	PROTOCOL_PORT: 1234,
-	PROTOCOL_MULTICAST_ADDRESS: '255.255.255.255' 
+	PROTOCOL_MULTICAST_ADDRESS: '255.255.255.255'
 }
+
+const argInstrument = 2;
 
 // Let's create a datagram socket. We will use it to send our UDP datagrams
 const s = dgram.createSocket('udp4');
 
 const activeSince = Date.now();
 
-var instruments = new Map();
+let instruments = new Map();
 	instruments.set("piano", "ti-ta-ti");
 	instruments.set("trumpet", "pouet");
 	instruments.set("flute", "trulu");
 	instruments.set("violin", "gzi-gzi");
 	instruments.set("drum", "boum-boum");
 
-// https://nodejs.org/api/process.html#process_process_env
-const musician = process.env.instrument
-// docker run res/musician -e instrument="piano"
-// docker run 1234 piano
+// https://nodejs.org/en/knowledge/command-line/how-to-parse-command-line-arguments/
+const musician = process.arg[argInstrument];
+// docker run res/musician piano
 
 // Create object and serialize it to JSON
 const obj = new Object();
 obj.instrument = musician;
 obj.sound = instruments.get(musician);
 obj.activeSince = activeSince;
+obj.uuid = uuidv4();
 
 const payload = JSON.stringify(obj);
 
@@ -35,6 +40,7 @@ setInterval(function(){
 		// Send the payload via UDP (multicast)
 		message = new Buffer(payload);
 		s.send(message, 0, message.length, protocol.PROTOCOL_PORT, protocol.PROTOCOL_MULTICAST_ADDRESS,
-		function(err, bytes) {console.log("Sending payload: " + payload + " via port " + s.address().port);
+		function(err, bytes) {
+			console.log("Sending payload: " + payload + " via port " + s.address().port);
 		});
 	}, 2000);
