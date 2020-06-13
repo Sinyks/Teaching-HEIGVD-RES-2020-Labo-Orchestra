d5d5fd1aa2024d10c8e9c9dcaf42cdbce95ad232
Sachouille <sacha.perdrizat@gmail.com>
2020-06-13 14:48:58 +0200

    add auditor package.json

--- docker/image-auditor/src/auditor.js
+++ docker/image-auditor/src/auditor.js
@@ -1,1 +1,11 @@
-FROM node:4.3.1
+let dgram = require('dgram');
+
+let server = dgram.createSocket('udp4');
+
+server.bind(1234, function () {
+  console.log('listen on port 1234');
+});
+
+server.on('message', (msg, rinfo) => {
+    console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
+});
